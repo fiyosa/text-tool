7b17f15d6729cca331bcd5ebdbc8d8cf220e6788
fiyosa <fiyosa97@gmail.com>
2025-06-22 21:38:50 +0700

    add: build folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'main.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'index.css') return 'main.css'
+          return 'assets/[name]-[hash].[ext]'
+        },
+      },
+    },
+  },
 })
