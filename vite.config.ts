6c8cb1a8199abb597d171d742bc02f695c0f3814
fiyosa <fiyosa97@gmail.com>
2025-06-28 17:45:40 +0700

    edit: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,39 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/text-tool/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: '/text-tool/', // optional
 
-  server: {
-    port: 3000,
-    host: 'localhost',
-  },
+    server: {
+      port: 3000,
+      host: 'localhost',
+    },
 
-  preview: {
-    port: 3001,
-    host: 'localhost',
-  },
+    preview: {
+      port: 3001,
+      host: 'localhost',
+    },
 
-  esbuild: {
-    legalComments: 'none',
-    drop: ['console'],
-  },
+    esbuild: {
+      legalComments: 'none',
+      drop: command === 'serve' ? [] : ['console'],
+    },
 
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-    manifest: false,
-    rollupOptions: {
-      output: {
-        entryFileNames: 'main.js',
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'index.css') return 'main.css'
-          return 'assets/[name]-[hash].[ext]'
+    build: {
+      outDir: 'docs',
+      emptyOutDir: true,
+      manifest: false,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'main.js',
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name === 'index.css') return 'main.css'
+            return 'assets/[name]-[hash].[ext]'
+          },
         },
       },
     },
-  },
+  }
 })
